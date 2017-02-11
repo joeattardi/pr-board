1affb9930aa06d6fd7861a79dc1e83711b85f69c
Joe Attardi <joe@attardi.net>
2017-02-11 17:10:15 -0500

    add Google Analytics

--- src/index.js
+++ src/index.js
@@ -12,6 +12,16 @@ import routes from './routes';
 import store from './store/index';
 import firebaseApp, { getAccessToken } from './firebase';
 
+if (NODE_ENV === 'production') {
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-80557105-2', 'auto');
+  ga('send', 'pageview');
+}
+
 Vue.use(VueRouter);
 Vue.use(VueResource);
 
